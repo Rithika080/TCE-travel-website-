bd5d2acb75021f69e9a4986135d06f37ac633ca7
Rithika080 <139517349+Rithika080@users.noreply.github.com>
2023-11-10 19:48:43 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const routes = require("./Routes/routes.js")
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use("/",routes);
+
+app.options( '*' , cors()) 
+
+//DB Connection
+mongoose.connect(
+    "mongodb+srv://rithikarshetty000:Rithika000@cluster0.tbxz5il.mongodb.net/Batch3"
+).then(()=>{
+    console.log("Data is connected successfully");
+})
+.catch((err)=>{
+    console.log(err,"Something went Wrong");
+});
+
+//Test API
+app.get("/test",(req,res)=>{
+    res.send("Hello Everyone, this is test API");
+});
+
+app.listen(4000,()=>{
+    console.log("Server is running on port 4000");
+});
